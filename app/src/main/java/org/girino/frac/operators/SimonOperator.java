23d9b3d0684b39673cd9c0fd70c7a4fdef6005f5
Nathan <nrrozzi@hotmail.com>
2026-09-22 16:51:08 -0700

    Refactor SimonOperator methods and variable usage
    Refactor SimonOperator class for clarity and efficiency.

--- app/src/main/java/org/girino/frac/operators/SimonOperator.java
+++ app/src/main/java/org/girino/frac/operators/SimonOperator.java
@@ -1,30 +1,21 @@
 package org.girino.frac.operators;
 
-/**
- * operator for mandelbrot sets.. 
- * 
- * Zn+1 = Zn^2 * (|Re(Zn)| + i*|Im(Zn)|)^2 + C
- * Z0 = 0
- * @author girino
- */
 public class SimonOperator extends FractalOperator {
 	
-	Complex tmp = new Complex();      // Zn^2
-	Complex absZ = new Complex();     // |Re|+i|Im|
-	Complex tmp2 = new Complex();     // (|Re|+i|Im|)^2
+	Complex tmp = new Complex();
+	Complex tmp2 = new Complex();
+	Complex absZ = new Complex();
 
-	protected void beforeIteration(int step, Complex Z, Complex C, int maxiter) {
-		// no C transform needed for +C formula
-	}
-
+	protected void beforeIteration(int step, Complex Z, Complex C, int maxiter) {}
 	@Override
-	protected void resumeIteration(int step, Complex Z, Complex C, int maxiter) {
-		// no C transform needed
-	}
+	protected void resumeIteration(int step, Complex Z, Complex C, int maxiter) {}
 
 	protected void step(int step, Complex Z, Complex C, int maxiter) {
 		// absZ = |Re(Z)| + i*|Im(Z)|
-		absZ.set(Math.abs(Z.getReal()), Math.abs(Z.getImag()));
+		double ax = Math.abs(Z.getReal());
+		double ay = Math.abs(Z.getImag());
+		absZ.setReal(ax);
+		absZ.setImag(ay);
 		
 		// tmp = Z * Z
 		tmp.ltimes(Z, Z);
@@ -32,10 +23,10 @@ public class SimonOperator extends FractalOperator {
 		// tmp2 = absZ * absZ
 		tmp2.ltimes(absZ, absZ);
 		
-		// tmp = tmp * tmp2 = Z^2 * (|Re|+i|Im|)^2
+		// tmp = tmp * tmp2
 		tmp.ltimes(tmp, tmp2);
 		
-		// Z = tmp + C
+		// Z = tmp + C  (your formula: z^2 * (|re|+i|im|)^2 + c)
 		Z.ladd(tmp, C);
 	}
 }

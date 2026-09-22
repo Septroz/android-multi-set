442b385a38eaef376cf547c3a6eee55f486a36aa
Nathan <nrrozzi@hotmail.com>
2026-09-22 16:56:12 -0700

    Refactor step method in SimonOperator
    Refactor step method to use new Complex instance for absZ and add scaling to prevent overflow.

--- app/src/main/java/org/girino/frac/operators/SimonOperator.java
+++ app/src/main/java/org/girino/frac/operators/SimonOperator.java
@@ -5,28 +5,29 @@ public class SimonOperator extends FractalOperator {
 	Complex tmp = new Complex();
 	Complex tmp2 = new Complex();
 	Complex absZ = new Complex();
+	Complex tmp3 = new Complex(); // extra temp to avoid aliasing bug
 
 	protected void beforeIteration(int step, Complex Z, Complex C, int maxiter) {}
 	@Override
 	protected void resumeIteration(int step, Complex Z, Complex C, int maxiter) {}
 
 	protected void step(int step, Complex Z, Complex C, int maxiter) {
-		// absZ = |Re(Z)| + i*|Im(Z)|
 		double ax = Math.abs(Z.getReal());
 		double ay = Math.abs(Z.getImag());
-		absZ.setReal(ax);
-		absZ.setImag(ay);
+		absZ = new Complex(ax, ay);
 		
-		// tmp = Z * Z
-		tmp.ltimes(Z, Z);
+		tmp.ltimes(Z, Z);              // Z^2
+		tmp2.ltimes(absZ, absZ);       // (|re|+i|im|)^2
+		tmp3.ltimes(tmp, tmp2);         // Z^2 * ...
 		
-		// tmp2 = absZ * absZ
-		tmp2.ltimes(absZ, absZ);
+		// scale down to keep it degree ~2 so it doesn't instantly blow up
+		// this keeps your shape but makes it renderable
+		double mag = Math.sqrt(tmp3.getReal()*tmp3.getReal() + tmp3.getImag()*tmp3.getImag());
+		if (mag > 1e6) {
+			tmp3.setReal(tmp3.getReal() / mag * 1e3);
+			tmp3.setImag(tmp3.getImag() / mag * 1e3);
+		}
 		
-		// tmp = tmp * tmp2
-		tmp.ltimes(tmp, tmp2);
-		
-		// Z = tmp + C  (your formula: z^2 * (|re|+i|im|)^2 + c)
-		Z.ladd(tmp, C);
+		Z.ladd(tmp3, C);
 	}
 }
